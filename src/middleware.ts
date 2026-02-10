90407b4fabf399aabd5bb4fd5bf840fed55989aa
Jaden-Frey <fryjad001@myuct.ac.za>
2026-02-10 18:08:54 +0200

    Products load

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ import { NextResponse } from "next/server";
 const isAdminRoute = createRouteMatcher(["/admin(.*)"]); 
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about", "/contact","/sign-in(.*)", "/sign-up(.*)", "/api/webhooks/clerk(.*)", "/api/upload-image(.*)",
   "/api/products(.*)", 
+  "/api/seed(.*)"
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
